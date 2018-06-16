2526528e526c50496a860faa11eec6a5872e47d7
Juan Bedoya <juansb827@gmail.com>
2018-06-16 17:19:00 -0500

    added client

---
+++ client/src/components/BookList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class BookList extends Component {
+  render() {
+    return (
+        <div>
+            <ul id="book-list">       
+                <li>Paul's Reading List</li>
+            </ul>
+        </div>
+    );
+  }
+}
+
+export default BookList;
--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb://paulvazo:test123@ds257640.mlab.com:57640/gql-test')
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
+
 app.use('/graphql', graphqlHTTP({
-    
+    schema: schema,
+    graphiql: true
 }))
 
 app.listen(4000, () =>{
