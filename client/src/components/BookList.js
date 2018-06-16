69814721251b757d419d6a63c53bee0b4b28bc93
Juan Bedoya <juansb827@gmail.com>
2018-06-16 18:53:51 -0500

    set up apollo

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo'
+const getBooksQuery = gql`
+    {
+        books{
+            name
+            id
+        }
+    }
+`
+
+
 
 
 class BookList extends Component {
-  render() {
-    return (
-        <div>
-            <ul id="book-list">       
-                <li>Paul's Reading List</li>
-            </ul>
-        </div>
-    );
-  }
+    displayBooks(){
+        var data = this.props.data;
+        if(data.loading){
+            return( <div>Loading books...</div> )
+        } else{
+            return data.books.map(book => {
+                return <li key={ book.id } >{ book.name }</li>
+            })
+        }
+    }
+
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <ul id="book-list">
+                    { this.displayBooks() }
+                </ul>
+            </div>
+        );
+    }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
